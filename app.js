d9ebcf8fd37326807459fa54f40592091fb8c7fe
Khayman King <khaymanaking@gmail.com>
2022-03-31 16:36:26 -0700

    succesfully exported default

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 // import functions and grab DOM elements
-
+import defaultExport from './data.js';
 // let state
-
+const countMap = defaultExport;
 // set event listeners 
   // get user input
+console.log(countMap);
   // use user input to update state 
   // update DOM to reflect the new state
