910a04577ee6700a280c7307a9b7c0027fa07bfc
Khayman King <khaymanaking@gmail.com>
2022-03-31 17:05:40 -0700

    showing on website however data needs work

--- app.js
+++ app.js
@@ -7,3 +7,28 @@ const countMap = defaultExport;
 console.log(countMap);
   // use user input to update state 
   // update DOM to reflect the new state
+const labels = Object.keys(countMap);
+
+
+const data = {
+    labels: labels,
+    datasets: [
+        {
+            label: 'Purchase Frequency',
+            backgroundColor: 'black',
+            borderColor: 'rbg(255, 99, 132)',
+            data: Object.values(countMap)
+        },
+    ]
+};
+
+const config = {
+    type: 'bar',
+    data: data,
+    options: {}
+};
+
+const myChart = new Chart(
+    document.getElementById('myChart'),
+    config
+);
