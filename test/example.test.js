919ad9e72b4195ddb103bd05344e169b2e842c78
Khayman King <khaymanak@gmail.com>
2022-04-06 10:17:33 -0700

    test for purchase frequency passed

--- test/example.test.js
+++ test/example.test.js
@@ -1,6 +1,6 @@
 // IMPORT MODULES under test here:
 // import { example } from '../example.js';
-
+import { customerFreq } from '../munge-utils.js';
 const test = QUnit.test;
 
 test('time to test a function', (expect) => {
@@ -15,4 +15,30 @@ test('time to test a function', (expect) => {
     //Expect
     // Make assertions about what is expected versus the actual result
     expect.equal(actual, expected);
+});
+
+
+
+
+test('takes in customer frequency and returns a countMap', (expect) => {
+    //Arrange
+    // Set up your arguments and expectations
+    const countMap = {
+        Often: 126,
+        Once: 107,
+        Daily: 142,
+        Yearly: 128,
+        Monthly: 123,
+        Weekly: 122,
+        Seldom: 129,
+        Never: 123,
+    };
+    const expected = countMap;
+    //Act 
+    // Call the function you're testing and set the result to a const
+    const actual = customerFreq();
+
+    //Expect
+    // Make assertions about what is expected versus the actual result
+    expect.deepEqual(actual, expected);
 });
