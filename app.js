dc09709c8d2fc034a719ca509560ff620c353a03
Khayman King <khaymanak@gmail.com>
2022-04-06 09:31:24 -0700

    added cool line chart

--- app.js
+++ app.js
@@ -1,35 +1,58 @@
-import { customerFreq } from './munge-utils.js'; 
+import { customerFreq, cool } from './munge-utils.js'; 
 
-const countMap = customerFreq();
-
-
-
-const labels = Object.keys(countMap);
+const frequencyCountMap = customerFreq();
+const coolCountMap = cool();
 
 
-const data = {
-    labels: labels,
+const frequencyLabels = Object.keys(frequencyCountMap);
+const coolLabels = Object.keys(coolCountMap);
+
+const frequencyData = {
+    labels: frequencyLabels,
     datasets: [{
         label: 'Purchase Frequency',
         backgroundColor: 'black',
         borderColor: 'rbg(255, 99, 132)',
-        data: Object.values(countMap)
+        data: Object.values(frequencyCountMap)
     }]
 };
 
-const config = {
+const frequencyConfig = {
     type: 'bar',
-    data: data,
+    data: frequencyData,
     options: {}
 };
 
+
 //eslint-disable-next-line
-const myChart = new Chart(
-    document.getElementById('myChart'),
-    config
+const myFrequencyChart = new Chart(
+    document.getElementById('myFrequencyChart'),
+    frequencyConfig
 );
 
 
+const coolData = {
+    labels: coolLabels,
+    datasets: [{
+        label: 'Purchase Frequency',
+        backgroundColor: 'black',
+        borderColor: 'rbg(255, 99, 132)',
+        data: Object.values(coolCountMap)
+    }]
+};
+
+const coolConfig = {
+    type: 'line',
+    data: coolData,
+    options: {}
+};
+
+
+//eslint-disable-next-line
+const myCoolChart = new Chart(
+    document.getElementById('myCoolChart'),
+    coolConfig
+);
 
 
 //TODO Line chart for cool factor, pie chart showing gender
